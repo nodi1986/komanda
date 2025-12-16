de1bf122e7469b68a1514f0447ef04519e621574
Sanjar-zdes <s1680598@gmail.com>
2025-12-16 09:22:25 +0500

    Bekzod change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import cafeImg from "./assets/cafe.png";
 import storeImg from "./assets/store.png";
+import Bekzod from "./assets/components/Bekzod"
 
 const App = () => {
   return (
     <div>
-      
+      <Bekzod/>
     </div>
   );
 };
+
 
 export default App;
